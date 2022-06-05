ecd5fa8f1896c397aafb58759431d685decd22a7
wbrasier <whit.brasier@gmail.com>
2022-06-05 19:57:16 +0000

    post rubocop

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,13 +1,10 @@
-#ask user for 2 numbers using Kernel.gets().chomp()
-#ask the user for operation to perform
-#performs the operation on the two numbers
-#output the result
-
-#answer = Kernel.gets().chomp()
-#Kernel.puts(answer)
+# ask user for 2 numbers using Kernel.gets().chomp()
+# ask the user for operation to perform
+# performs the operation on the two numbers
+# output the result
 
 def prompt(message)
-  Kernel.puts"=> #{message}"
+  Kernel.puts "=> #{message}"
 end
 
 def valid_number?(num)
@@ -24,50 +21,50 @@ def operation_to_message(op)
     'Multiplying'
   when '4'
     'Dividing'
-  end 
+  end
 end
-#because the case statement is the last evaluated expression it is what will be returned
+# case statement is the last evaluated expression, so it is returned
 
 prompt("Welcome to Calculator! Enter your name:")
 
 name = ''
 loop do
   name = Kernel.gets.chomp
-  
+
   if name.empty?
-    prompt"Make sure to use a valid name."
+    prompt "Make sure to use a valid name."
   else
     break
   end
 end
 
-prompt"Hi #{name}!"
+prompt "Hi #{name}!"
 
-loop do #main loop
+loop do
   number1 = nil
   loop do
-    prompt"What's the first number?"
+    prompt "What's the first number?"
     number1 = Kernel.gets.chomp
-    
-    if valid_number?number1
+
+    if valid_number? number1
       break
     else
-      prompt"Hmm... that doesn't look like a valid number"
-    end 
-  end
-  
-  number2 = nil
-  loop do
-    prompt"What's the second number?"
-    number2 = Kernel.gets.chomp
-    
-    if valid_number?number2
-      break
-    else
-      prompt"Hmm... that doesn't look like a valid number"
+      prompt "Hmm... that doesn't look like a valid number"
     end
   end
-  
+
+  number2 = nil
+  loop do
+    prompt "What's the second number?"
+    number2 = Kernel.gets.chomp
+
+    if valid_number? number2
+      break
+    else
+      prompt "Hmm... that doesn't look like a valid number"
+    end
+  end
+
   operator_prompt = <<-MSG
     What operation would you like to perform?
     1) add
@@ -76,36 +73,33 @@ loop do #main loop
     4) divide
   MSG
   prompt(operator_prompt)
-  
+
   operator = ''
   loop do
     operator = Kernel.gets.chomp
-    
-    if %w( 1 2 3 4).include?operator
+
+    if %w(1 2 3 4).include? operator
       break
     else
-      prompt"Must choose 1, 2, 3, or 4"
+      prompt "Must choose 1, 2, 3, or 4"
     end
   end
-  
-  prompt"#{operation_to_message(operator)} the two numbers..."
-  
-  result = case operator
-           when '1'
-             result = number1.to_i + number2.to_i
-           when '2'
-             result = number1.to_i - number2.to_i
-           when '3'
-             result = number1.to_i * number2.to_i
-           when '4'
-              result = number1.to_f / number2.to_f
-  end
-  
-  prompt"The result is #{result}"
-  
-  prompt"Do you want to perform another operation?(Y to calculate again)"
+
+  prompt "#{operation_to_message(operator)} the two numbers..."
+  result =  case operator
+            when '1'
+              number1.to_i + number2.to_i
+            when '2'
+              number1.to_i - number2.to_i
+            when '3'
+              number1.to_i * number2.to_i
+            when '4'
+              number1.to_f / number2.to_f
+            end
+  prompt "The result is #{result}"
+  prompt "Do you want to perform another operation?(Y to calculate again)"
   answer = Kernel.gets.chomp
-  break unless answer.downcase.start_with?'y'
+  break unless answer.downcase.start_with? 'y'
 end
 
-prompt"Thank you for using the calculator! Good bye!"
+prompt "Thank you for using the calculator! Good bye!"

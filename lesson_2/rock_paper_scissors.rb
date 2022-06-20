eb7b2f049862446238235d83ec4849d088f1e8ed
wbrasier <whit.brasier@gmail.com>
2022-06-20 00:14:55 +0000

    bonus features added to RPS

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,13 +1,35 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES_WORDS = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+
+VALID_CHOICES_LETTERS = ['r', 'p', 'sc', 'l', 'sp']
+
+WINNING_MOVES = { 'rock' => ['lizard', 'scissors'],
+                  'paper' => ['rock', 'spock'],
+                  'scissors' => ['paper', 'lizard'],
+                  'lizard' => ['spock', 'paper'],
+                  'spock' => ['scissors', 'rock'] }
 
 def prompt(message)
   puts("=> #{message}")
 end
 
+def letters_to_words(letter)
+  move = case letter
+         when 'r'
+           'rock'
+         when 'p'
+           'paper'
+         when 'sc'
+           'scissors'
+         when 'l'
+           'lizard'
+         when 'sp'
+           'spock'
+         end
+  move
+end
+
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  WINNING_MOVES[first].each.include?(second)
 end
 
 def display_results(player, computer)
@@ -20,25 +42,65 @@ def display_results(player, computer)
   end
 end
 
-loop do
-  choice = ''
+def update_score(scores, player, computer)
+  if win?(player, computer)
+    scores[:player] += 1
+  elsif win?(computer, player)
+    scores[:computer] += 1
+  end
+end
 
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
+def display_score(scores)
+  prompt("Your current score is: #{scores[:player]}.")
+  prompt("The computer's score is : #{scores[:computer]}.")
+end
 
-    if VALID_CHOICES.include?(choice)
+loop do # main loop
+  score = { player: 0,
+            computer: 0 }
+  loop do # loop of each round
+    choice_letter = ''
+
+    loop do
+      output = <<-MSG
+      => Pick one and type coordinating letters:
+      => rock (r)
+      => paper (p)
+      => scissors (sc)
+      => lizard (l)
+      => spock (sp)
+      MSG
+
+      puts output
+
+      choice_letter = gets.chomp.downcase
+
+      if VALID_CHOICES_LETTERS.include?(choice_letter)
+        break
+      else
+        prompt("That is not a valid choice.")
+      end
+    end
+
+    choice_word = letters_to_words(choice_letter)
+
+    computer_choice = VALID_CHOICES_WORDS.sample
+
+    puts "You chose: #{choice_word}; computer chose: #{computer_choice}."
+
+    display_results(choice_word, computer_choice)
+    update_score(score, choice_word, computer_choice)
+    display_score(score)
+
+    # exits the loop if somebody has 3 points
+    if score[:player] == 3
+      prompt("You are the ultimate champ!")
       break
-    else
-      prompt("That is not a valid choice.")
+    elsif score[:computer] == 3
+      prompt("The computer is the ultimate champ!")
+      break
     end
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  puts "You chose: #{choice}; computer chose: #{computer_choice}."
-
-  display_results(choice, computer_choice)
 
   prompt("Do you want to play again?")
   answer = gets.chomp

c1e70677b74072fa58875a34f0cb2baf1174df9c
wbrasier <whit.brasier@gmail.com>
2022-06-20 23:18:49 +0000

    updated calculator internationalized

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -3,92 +3,97 @@
 # performs the operation on the two numbers
 # output the result
 
-=begin
-def prompt(message)
-  Kernel.puts "=> #{message}"
-end
-=end
+LANGUAGE = 'en'
+
 require 'yaml'
+MESSAGES = YAML.load_file("config.calculator.yml")
 
-conf = YAML.load_file("config.calculator.yml")
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
 
+def prompt(key)
+  message = messages(key, LANGUAGE)
+  puts "=> #{message}"
+end
 
 def number?(num)
-    if num.include?('.')
-      num.to_f.to_s == num
-    else 
-      num.to_i.to_s == num 
-    end
+  if num.include?('.')
+    num.to_f.to_s == num
+  else
+    num.to_i.to_s == num
+  end
 end
 
 def operation_to_message(op)
-  conf = YAML.load_file("config.calculator.yml")
   operator_string = case op
                     when '1'
-                      conf.fetch(:adding)
+                      messages('adding')
                     when '2'
-                      conf.fetch(:subtracting)
+                      messages('subtracting')
                     when '3'
-                      conf.fetch(:multiplying)
+                      messages('multiplying')
                     when '4'
-                      conf.fetch(:dividing)
+                      messages('dividing')
                     end
-  operator_string 
+  operator_string
 end
 
 name = ''
 loop do
-  puts conf.fetch(:welcome)
-  name = Kernel.gets.chomp
+  prompt('welcome')
+  name = gets.chomp
 
   if name.empty?
-    puts conf.fetch(:valid_name)
+    prompt('valid_name')
   else
     break
   end
 end
 
-puts conf.fetch(:hi) + " #{name}!"
+puts format(messages('hi'), name: name)
 
 loop do
   number1 = nil
   loop do
-    puts conf.fetch(:retrieve_num_1)
-    number1 = Kernel.gets.chomp
+    prompt('retrieve_num_1')
+    number1 = gets.chomp
 
     if number? number1
       break
     else
-      puts conf.fetch(:invalid_num)
+      prompt('invalid_num')
     end
   end
 
   number2 = nil
   loop do
-    puts conf.fetch(:retrieve_num_2)
-    number2 = Kernel.gets.chomp
+    prompt('retrieve_num_2')
+    number2 = gets.chomp
 
     if number? number2
       break
     else
-      puts conf.fetch(:invalid_num)
+      prompt('invalid_num')
     end
   end
 
-puts conf.fetch(:operator_prompt)
+prompt('operator_prompt')
 
   operator = ''
   loop do
-    operator = Kernel.gets.chomp
+    operator = gets.chomp
 
     if %w(1 2 3 4).include? operator
       break
     else
-      puts conf.fetch(:must_choose)
+      prompt('must_choose')
     end
   end
 
-  puts "#{operation_to_message(operator)} " + conf.fetch(:numbers_action)
+operation = operation_to_message(operator)
+puts format(messages('numbers_action'), operation: operation)
+
   result =  case operator
             when '1'
               number1.to_i + number2.to_i
@@ -98,15 +103,17 @@ puts conf.fetch(:operator_prompt)
               number1.to_i * number2.to_i
             when '4'
               if number2.to_i == 0
-                puts conf.fetch(:no_zero)
+                prompt('no_zero')
               else
                 number1.to_f / number2.to_f
               end
             end
-  puts conf.fetch(:result) + " #{result}" unless operator == '4'
-  puts conf.fetch(:another)
-  answer = Kernel.gets.chomp
+
+  puts format(messages('result'), result: result)
+
+  prompt('another')
+  answer = gets.chomp
   break unless answer.downcase.start_with? 'y'
 end
 
-puts conf.fetch(:goodbye)
+prompt('goodbye')
